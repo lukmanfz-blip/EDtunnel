ba080902a0915fa3d673d96d2db8e325fb41a6ac
lukmanfz-blip <121635796+lukmanfz-blip@users.noreply.github.com>
2024-05-18 01:57:24 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,9 +3,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'ded04476-945d-46f9-9d3d-1a4132236f15';
+let userID = '3733f927-9f37-4733-894f-222e8ea276c3';
 
-const พร็อกซีไอพีs = ['168.138.185.253'];
+const พร็อกซีไอพีs = ['103.165.126.65'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
