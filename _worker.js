5d4ac7222737b1fd2d25749d8b3a84888d3414df
lukmanfz-blip <121635796+lukmanfz-blip@users.noreply.github.com>
2024-05-20 07:35:47 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'be08a49d-fe36-43ae-88b2-65dc98f77d5d';
 
-const พร็อกซีไอพีs = ['103.154.138.43'];
+const พร็อกซีไอพีs = ['8.219.170.232'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
